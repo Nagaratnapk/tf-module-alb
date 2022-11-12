15d479d5d75a37e4493ff85d5e97886d8eeb4eeb
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MBP.attlocal.net>
2022-11-11 19:10:59 -0800

    update

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@ output "ALB_ARN" {
     value = aws_lb.alb.arn
 }
 
+output "LISTENER_ARN" {
+    value = aws_lb_listener.private.*.arn
+}
+
+output "ALB_ADDRESS" {
+    value = aws_lb.alb.dns_name
+}
